84a79e91cfa35c589f55b3faec1a8da485ba2605
Pawel Gajewski <lubiluk@gmail.com>
2021-07-29 09:13:42 +0200

    ReLu activation

--- neat/activations.py
+++ neat/activations.py
@@ -10,13 +10,16 @@ def sigmoid(x):
 def tanh(x):
     return torch.tanh(x)
 
+def relu(x):
+    return torch.relu(x)
 
 class Activations:
 
     def __init__(self):
         self.functions = dict(
             sigmoid=sigmoid,
-            tanh=tanh
+            tanh=tanh,
+            relu=relu
         )
 
     def get(self, func_name):
